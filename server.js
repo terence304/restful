3ebfa4a7f2603aa47097ee80bb86b26ef1a12171
terence304 <terence304@gmail.com>
2017-09-10 19:21:47 -0700

    recover to template

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-
+    
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
